e3f4822dd430f2006f2b7c6ac2012f40ee5bd785
Yevgeniy Goncharov <g.yevgeniy.p@gmail.com>
2023-05-03 20:41:54 +0600

    get timedate funx

--- test-ports.sh
+++ test-ports.sh
@@ -1,5 +1,20 @@
 #!/bin/bash -e
 # Ref: https://www.redhat.com/sysadmin/stop-using-telnet-test-port
+
+# Sys env / paths / etc
+# ---------------------------------------------------------------------\
+PATH=$PATH:/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin
+SCRIPT_PATH=$(cd `dirname "${BASH_SOURCE[0]}"` && pwd)
+cd $SCRIPT_PATH
+
+# ---------------------------------------------------------------------\
+
+# Get datetime
+# ---------------------------------------------------------------------\
+get_datetime() {
+  echo $(date '+%Y-%m-%d_%H:%M:%S')
+}
+
 if [ -n "$1" ] && [ -f "$1" ]; then
   while read -r line; do
     machine=$(echo "$line"| cut -d' ' -f1)|| exit 100
@@ -8,9 +23,9 @@ if [ -n "$1" ] && [ -f "$1" ]; then
     IFS=","
     for port in $ports; do
       if  (echo >/dev/tcp/"$machine"/"$port") >/dev/null 2>&1; then
-        echo "OK: $machine -> $port"
+        echo "OK ( $(get_datetime) ): $machine -> $port"
       else
-        echo "ERROR: $machine -> $port"
+        echo "ERROR ( $(get_datetime) ): $machine -> $port"
       fi
     done
     IFS=$OLD_IFS
